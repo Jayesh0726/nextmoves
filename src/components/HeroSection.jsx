2d036803570984db86ffaef5ca5ab2d2fb153a72
Jayesh Padwal <jayeshpadwal09@gmail.com>
2026-01-29 01:51:18 +0530

    feat: integrate Spline scene into HeroSection component

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,23 +2,9 @@ import { useState, useEffect, memo } from 'react';
 import Spline from '@splinetool/react-spline';
 
 const HeroSection = memo(function HeroSection() {
-  const [sceneUrl, setSceneUrl] = useState('');
   const [isMobile, setIsMobile] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    // Use cached scene URL to avoid repeated API calls
-    const SPLINE_SCENE_URL = 'https://prod.spline.design/Z6pKUxTWimjVS57c/scene.splinecode';
-    
-    // Check if URL is cached in sessionStorage
-    const cachedUrl = sessionStorage.getItem('splineSceneUrl');
-    if (cachedUrl) {
-      setSceneUrl(cachedUrl);
-    } else {
-      setSceneUrl(SPLINE_SCENE_URL);
-      sessionStorage.setItem('splineSceneUrl', SPLINE_SCENE_URL);
-    }
-    
     // Detect if device is mobile/touch
     const checkMobile = () => {
       setIsMobile(
@@ -35,30 +21,27 @@ const HeroSection = memo(function HeroSection() {
 
   return (
     <div 
-      className="w-screen h-screen overflow-x-hidden relative"
+      className="w-screen h-screen overflow-x-hidden relative bg-black"
       style={{ willChange: 'transform' }}
     >
-      {sceneUrl && (
-        <div 
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            width: '100%',
-            height: '100%',
-            // Disable pointer events on mobile to allow scrolling
-            pointerEvents: isMobile ? 'none' : 'auto',
-          }}
-        >
-          <Spline 
-            scene={sceneUrl}
-            onLoad={() => setIsLoading(false)}
-          />
-        </div>
-      )}
+      <div 
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          pointerEvents: isMobile ? 'none' : 'auto',
+        }}
+      >
+        <Spline 
+          scene="/scene.splinecode"
+        />
+      </div>
     </div>
   );
 });
+
 
 export default HeroSection;
 
